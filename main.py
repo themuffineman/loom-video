37b8815c17c2692fe3ed0f51d3866f10be74ffd5
themuffineman <petrusheya@gmail.com>
2025-09-01 00:41:46 +0200

    testing framework

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import sync_playwright, expect
 
-def test_has_title(page: Page):
+def test_has_title(page):
     page.goto("https://playwright.dev/")
-
-    # Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Playwright"))
 
-def test_get_started_link(page: Page):
+def test_get_started_link(page):
     page.goto("https://playwright.dev/")
-
-    # Click the get started link.
     page.get_by_role("link", name="Get started").click()
-
-    # Expects page to have a heading with the name of Installation.
     expect(page.get_by_role("heading", name="Installation")).to_be_visible()
+
+if __name__ == "__main__":
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)  # headless=False to see the browser
+        page = browser.new_page()
+        test_has_title(page)
+        test_get_started_link(page)
+        browser.close()
